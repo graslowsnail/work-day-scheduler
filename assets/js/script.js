9f575f25bf56175e103f0408c5a6d75211495edb
graslowsnail <graslowsnail@gmail.,com>
2022-09-15 22:23:05 -0500

    commit after text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,1 @@
-var tasks = {};
-
-var createTask = function (taskText, taskTime) {
-    var taskLi = $("<li>");
-    var taskSpan = $("<span>")
-        .addClass("m-1")
-        .text(taskTime);
-    var taskP = $("<p>")
-        .addClass("m-1")
-        .text(taskText);
-
-    taskLi.append(taskSpan, taskP);
-
-    $("#list-" + taskLi).append(taskLi);
-};
-
-createTask();
-
+$("<p>Test</p>").appendTo("#hour-1");
